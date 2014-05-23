a243824b853f5624fad3ee839faed00c03ba7665
Siriustar <jim.davis720@gmail.com>
2014-05-23 00:32:38 -0400

    Commit of completed cachematrix.R for R Programming assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Description of code:
+## Getting a matrix value from user-instantiated variable,
+## sets it for use outside the current function's environment,
+## and solves for the inverse of the given matrix
+## (caught from the function makeCacheMatrix).
 
-## Write a short comment describing this function
+## makeCacheMatrix:
+## Gets a matrix value from user input,
+## sets it for use outside the current function's environment
+## by passing a list of global variables
+## to the below function 'cacheSolve'
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve:
+## Getting the inverse of the matrix called into
+## the above function 'makeCacheMatrix' by using a list
+## of its global variables
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
 }
